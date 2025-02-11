2da3843626e52bc4945101ff55dd6453321b2f01
Kartik Sengar <kartiksengar1@gmail.com>
2025-02-11 15:24:46 +0530

    varletconstex

---
+++ JS Basic/Day 3/eg 2.js
@@ -0,0 +1,11 @@
+var a =10;
+function f() {
+    var b=20;
+    console.log(b);
+    // return b; this will only be needed to be shown if you want to remove undefined from your code.
+}
+
+f();
+console.log(a);
+
+console.log(f());
---
+++ JS Basic/Day 3/eg code.js
@@ -0,0 +1,25 @@
+// let a=10;
+// console.log(a); : this will give us the output as - 10
+
+// let a=10;
+// console.log(a);
+// let a; : if we are typing this only, then it will give us the 'Reference Error', as it has not yet been initialised
+
+// b=10;
+// console.log(b);
+// var b; : this will also work, by giving the output - 10
+
+// console.log(c);
+// var c=10; : this will give us as undefined
+
+// console.log(d);
+// const d=10; - this will also give us the Reference Error as the constant d, has not yet been initialised
+
+// const e = 129312;
+// console.log(e); : this will give us the output successfully as - 129312
+
+// const e;
+// console.log(e);
+// e = 12312; : This will give us the SyntaxError: Missing initializer in const declaration
+
+// This is how Hoisting works in JS. We have to put the declarations and the initialisations in the top scope.
